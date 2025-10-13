d23b51b5c0873a631d48a2cab158cf4be22fd134
hugogoncalvez <hugogoncalvez@gmail.com>
2025-10-13 18:42:30 -0300

    milesima modificacion

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,22 +1,23 @@
-use tauri_plugin_log::{Builder as LogBuilder, LogTarget};
+use tauri_plugin_log::Builder as LogBuilder;
 use log::LevelFilter;
 
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
+            // Inicializamos el plugin de logging globalmente
             app.handle().plugin(
                 LogBuilder::default()
-                    .targets([
-                        LogTarget::LogDir,
-                        LogTarget::Stdout,
-                        LogTarget::Webview,
-                    ])
+                    // Nivel de log mínimo a registrar
                     .level(LevelFilter::Info)
+                    // Guarda los logs en el directorio por defecto de la app
                     .build(),
             )?;
+
             Ok(())
         })
+        // Otros plugins
         .plugin(tauri_plugin_process::init())
+        // Inicia la app Tauri
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("Error while running Tauri application");
 }
