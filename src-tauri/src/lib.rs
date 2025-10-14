13e6be7362406c66ff0af6955ec36a3198ccd481
hugogoncalvez <hugogoncalvez@gmail.com>
2025-10-13 21:22:30 -0300

    fix(tauri): Re-add http plugin initialization in lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,6 @@
 use tauri_plugin_log::{Builder as LogBuilder, Target, TargetKind};
 use log::LevelFilter;
+use tauri_plugin_http::init as http_plugin_init;
 
 pub fn run() {
     tauri::Builder::default()
@@ -12,7 +13,7 @@ pub fn run() {
                     // Establecemos explícitamente los destinos de los logs
                     .target(Target::new(TargetKind::Stdout))      // consola del terminal
                     .target(Target::new(TargetKind::Webview))     // consola del webview
-                    .target(Target::new(TargetKind::LogDir { file_name: Some("app.log".into()) }))
+                    .target(Target::new(TargetKind::LogDir { file_name: Some("app".into()) }))
                     .build(),
             )?;
 
@@ -20,6 +21,7 @@ pub fn run() {
         })
         // Otros plugins
         .plugin(tauri_plugin_process::init())
+        .plugin(http_plugin_init())
         // Inicia la app Tauri
         .run(tauri::generate_context!())
         .expect("Error while running Tauri application");
